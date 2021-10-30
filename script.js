6fce33731a569453b42a770d0028c13a5e5a0eb0
francienevaz <fran.vfraga@gmail.com>
2021-10-30 17:57:40 -0300

    update

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector('.background');
+let isJumping = false
+let position = 0;
 
-function handleKeyUp (event) {
-    if (event.keyCode === 32) {
-        jump();
+function handleKeyUp (event){
+    if (event.keyCode === 32){
+        if (!isJumping) {
+          jump();  
+        }
+        
     }
 }
-
 function jump () {
-    let position = 0;
+    
+    isJumping = true;
 
     let upInterval = setInterval(() => {
-      if (position >= 150) {
-          clearInterval(upInterval);
-      } else {    
-    
-      
-      //subindo
-        position += 20;
+        if(position >= 150){
+            clearInterval(upInterval);
 
-       dino.style.bottom = position + 'px';
+            //descendo
+            let downInterval = setInterval(() => {
+                
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                   position -= 20;
+                dino.style.bottom = position + "px"; 
+                }
 
-    }, 20);
-}}
+            },20);
+        } else {
+            // subindo
+            position += 20;
+            dino.style.bottom = position + "px";
+        }
+    },20);
 
-document.addEventListener('keyup', handleKeyUp);
+}
+function createCactus () {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
-/*document.addEventListener('keyup', function () {
-    console.log ('pressionou uma tecla')
-})
-*/
+    console.log(randomTime)
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game Over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over"> Fim de Jogo</h1>'
+
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + "px"; 
+        }
+    },20);
+
+    setTimeout(createCactus, randomTime)
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp)
